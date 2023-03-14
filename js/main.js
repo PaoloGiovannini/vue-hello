be1802fb76addcfbea31bbc261bc528179a06e06
Paolo Giovannini <giovannini13@gmail.com>
2023-03-14 14:21:56 +0100

    implentato bottone per le foto

--- js/main.js
+++ js/main.js
@@ -4,7 +4,18 @@
     data() {
       return {
         message: 'Ciao mondo, sto usando Vue!',
-        picture :'https://picsum.photos/200'
+        picture :'https://picsum.photos/200',
+        picture_2: 'img/cover-1.webp',
+        display: 'display-block'
       }
-    }
+    },
+    methods: {
+        cambiaDisplay() {
+          if (this.display == 'display-block') {
+            this.display = 'display-none';
+          } else {
+            this.display = 'display-block';
+          }
+        }
+      }
   }).mount('#app')
